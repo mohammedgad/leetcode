c4af406b56ddd6a64924119d52f97f090c87f6fc
Mohammed Gad <mohammedgad7@gmail.com>
2020-08-20 01:19:56 +0200

    reverse linked list iterative

---
+++ reverse_linked_list_iterative.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class ReverseLinkedListIterative
+  # Definition for singly-linked list.
+  # class ListNode
+  #     attr_accessor :val, :next
+  #     def initialize(val = 0, _next = nil)
+  #         @val = val
+  #         @next = _next
+  #     end
+  # end
+  # @param {ListNode} head
+  # @return {ListNode}
+  def reverse_list(head)
+    node = head
+    arr = []
+    until node.nil?
+      arr.push node.val
+      node = node.next
+    end
+
+    node = head
+    until arr.empty?
+      node.val = arr.pop
+      node = node.next
+    end
+    head
+  end
+end
