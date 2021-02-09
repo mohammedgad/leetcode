15c7b81e689c4d73ba542b0843babd0624c926b7
Mohamed Gad <mohamed.gad@concentrical.com>
2021-02-09 11:03:43 +0200

    mean_of_array_after_removing_some_elements

---
+++ mean_of_array_after_removing_some_elements.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class MeanofArrayAfterRemovingSomeElements
+  def trim_mean(arr)
+    arr = arr.sort!
+    min_max_length = arr.length * 0.05
+    
+    arr = arr[min_max_length..arr.length-1-min_max_length]
+    
+    arr.sum.to_f / arr.length
+  end
+end
