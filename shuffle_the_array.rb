087b517832797e3a24288623256de4feb6c322f0
Mohamed Gad <mohamed.gad@concentrical.com>
2021-02-10 11:53:30 +0200

    shuffle_the_array

---
+++ shuffle_the_array.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class ShuffletheArray
+    # @param {Integer[]} nums
+    # @param {Integer} n
+    # @return {Integer[]}
+    def shuffle(nums, n)
+        i = 0
+        j = 0
+        new_arr = []
+        while(i < nums.length/2)
+            new_arr[j] = nums[i]
+            new_arr[j+1] = nums[i+n]
+            i = i + 1
+            j = j + 2
+        end
+        new_arr
+    end
+end
