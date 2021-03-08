70941ee8f34ac0b394a21ffecefcb58aa89dfa40
Mohamed Gad <mohammedgad7@gmail.com>
2021-03-08 09:35:04 +0200

    same_tree

---
+++ same_tree.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class SameTree
+    # Definition for a binary tree node.
+    # class TreeNode
+    #     attr_accessor :val, :left, :right
+    #     def initialize(val = 0, left = nil, right = nil)
+    #         @val = val
+    #         @left = left
+    #         @right = right
+    #     end
+    # end
+    # @param {TreeNode} p
+    # @param {TreeNode} q
+    # @return {Boolean}
+    def is_same_tree(p, q)
+        return false if p&.val != q&.val
+        return true if p&.val.nil? && q&.val.nil?
+        is_same_tree(p.left, q.left) && is_same_tree(p.right, q.right)
+    end
+end
