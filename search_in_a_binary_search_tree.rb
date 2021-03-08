0ad3078538cedff60405c5f716ff39fc2f096836
Mohamed Gad <mohammedgad7@gmail.com>
2021-03-08 09:46:30 +0200

    search_in_a_binary_search_tree

---
+++ search_in_a_binary_search_tree.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class SearchInABinarySearchTree
+    # Definition for a binary tree node.
+    # class TreeNode
+    #     attr_accessor :val, :left, :right
+    #     def initialize(val = 0, left = nil, right = nil)
+    #         @val = val
+    #         @left = left
+    #         @right = right
+    #     end
+    # end
+    # @param {TreeNode} root
+    # @param {Integer} val
+    # @return {TreeNode}
+    def search_bst(root, val)
+        return if root.nil?
+        return root if root.val == val
+        search_bst(root.right, val) || search_bst(root.left, val)
+    end
+end
