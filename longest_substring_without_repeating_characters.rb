4cd0426687929cfd5207f5b80bfc568e7d5155ed
Mohamed Gad <mohammedgad7@gmail.com>
2021-07-18 22:06:20 +0200

    longest_substring_without_repeating_characters

---
+++ longest_substring_without_repeating_characters.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+class LongestSubstringWithoutRepeatingCharacters
+  # @param {String} s
+  # @return {Integer}
+  def length_of_longest_substring(s)
+    # steps
+    # extract unique substrings (sliding window)
+    # get longest substring
+
+    # return zero if s is an empty string
+    return 0 if s == ''
+
+    strings = []
+    i = 0
+    while i < s.length
+      substring = +''
+      j = i
+      while j < s.length
+        break if substring.include? s[j]
+
+        substring << s[j]
+        j += 1
+      end
+      strings.push substring
+      i += 1
+    end
+    longest_substring = strings.max_by { |n| n.length }
+    longest_substring.length
+  end
+end
